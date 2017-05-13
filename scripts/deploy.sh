746588761716715b6c2174ede90f6e888133cba6
Nathan Marley <nathan.marley@gmail.com>
2017-05-13 13:33:47 -0700

    deploy via travis (#4)
    * deploy via travis
    
    * only deploy necessary code

---
+++ scripts/deploy.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+set -ex
+
+DEPLOY_ENV="$1"
+# echo $TRAVIS_BUILD_DIR
+
+# TODO: staging environment
+if [ "$DEPLOY_ENV" = "production" ]; then
+  rsync -vzrlptD \
+    --exclude deploy_ed25519.enc \
+    --exclude .git \
+    --exclude .gitignore \
+    --exclude .travis.yml \
+    --exclude node_modules \
+    --exclude README.md \
+    --exclude package.json \
+    --exclude scripts \
+    --delete-after \
+    . deploy@proposal.dash.org:/var/www/govobject-proposal/
+fi
