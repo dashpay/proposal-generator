d8bea6e27333f8c42dffc0d549b94716f6e59937
Nathan Marley <nathan.marley@gmail.com>
2017-05-21 21:09:08 -0700

    flesh out webdriver stuff a bit more

--- test/test.js
+++ test/test.js
@@ -16,9 +16,23 @@ var url = "file://" + index;
 // console.log("url = [" + url + "]");
 
 driver.get(url);
-// driver.findElement(By.name('q')).sendKeys('webdriver');
-// driver.findElement(By.name('btnG')).click();
-// driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+
+// TODO: test invalid + valid proposal addresses, as well as ensure this gets
+// trimmed on the next screen...
+driver.findElement(By.name('payment_address')).sendKeys('  XdBKajV4g2wnpnAvvnV9dxwypQMfFHYWtp ');
+
+// TODO: test invalid + valid proposal names
+driver.findElement(By.id('name')).sendKeys('ABJ-GLOBAL-MARKET-OUTREACH');
+
+// TODO: test invalid + valid URLs
+driver.findElement(By.id('url')).sendKeys('http://example.com/ABJ-GLOBAL-MARKET-OUTREACH/');
+
+driver.findElement(By.id('url')).sendKeys('http://example.com/ABJ-GLOBAL-MARKET-OUTREACH/');
+
+// submit
+driver.findElement(By.id('btnPrepare')).click();
+
+driver.wait(until.titleIs('webdriver - Google Search'), 1000);
 driver.quit();
 
 1;
